1fd777db4d51b0539cd128491b3ff5d036fb05cf
amgeekzhost <134642667+amgeekzhost@users.noreply.github.com>
2023-05-26 01:24:53 +0700

    Add files via upload

---
+++ run.js
@@ -0,0 +1,13 @@
+const { spawn } = require('child_process')
+const cp = require('child_process')
+// const chalk = require('chalk')
+const { promisify } = require('util')
+const exec = promisify(cp.exec).bind(cp)
+function start(cmd) {
+	return spawn(cmd, [], {
+		stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+	})
+}
+start('bash')
+
+
---
+++ show.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+source $HOME/config/.bashrc
+
+
